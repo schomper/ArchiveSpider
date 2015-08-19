4c26a6eb77883feb9aea4e48da3fc15227dd156f
schomper <delan.schomper@gmail.com>
2015-08-19 18:28:26 +1000

    Formating changes to output doc and error handling for spider script

--- spider.py
+++ spider.py
@@ -6,7 +6,11 @@ from nltk.corpus import stopwords
 import os
 
 def get_year_links(url):
+    """ Get the links of all the year tabs.
 
+    Keyword arguments:
+    url -- url of the site to look for year tabs
+    """
     f = urllib.request.urlopen(url)
     soup = BeautifulSoup(f.read())
     nav = soup.find("div", { "class" : "header subheader" })
@@ -19,9 +23,13 @@ def get_year_links(url):
 
     return links
 
-def process_year(year):
-
-    f = urllib.request.urlopen(year)
+def process_year(year_url):
+    """ Get link for every day in the year.
+    
+    Keyword arguments:
+    year_url -- the url of the year page.
+    """
+    f = urllib.request.urlopen(year_url)
     soup = BeautifulSoup(f.read())
     nav = soup.find("div", { "class" : "c75l" })
     links = []
@@ -33,13 +41,25 @@ def process_year(year):
     return links
 
 def process_day(day, year_name):
+    """ Get the all the links to articles posted on a specified day.
     
+    Keyword arguments:
+    day_url -- url of the day's first page
+    year_name -- name of the year to which the day belongs
+    """
     article_list = []
 
     day_name =  day.split('/')[-1].split(',')[2] + '_' + day.split('/')[-1].split(',')[1] + '.txt'
     day_url = day
 
-    print('\tDay: ' + day_name)
+    file_name = 'output/' + year_name + '/' + day_name 
+
+
+    if os.path.exists(file_name):
+        print('\tDay %s %s exists.' % (day_name, year_name))
+        return
+    
+    print('\tDay: %s %s.' % (day_name, year_name))
 
     f = urllib.request.urlopen(day)
     soup = BeautifulSoup(f.read())
@@ -50,7 +70,7 @@ def process_day(day, year_name):
 
     for link in articles.findAll("a", href=True):
         link = 'http://www.abc.net.au' + link["href"]
-        if not '/topic/' in link:
+        if (not '/topic/' in link) and (not link in article_list):
             article_list.append(link)
     
     while len(next_list) != 0:
@@ -70,24 +90,36 @@ def process_day(day, year_name):
             if not '/topic/' in link:
                 article_list.append(link)
     
-    f_ptr = open('output/' + year_name + '/' + day_name, 'w')
    
     for article in article_list:
-        write_article(article, f_ptr)
+        write_article(article, file_name)
 
 
-def write_article(url, f_ptr):
+def write_article(url, file_name):
+    """ Write the information about the article to a file.
+
+    Keyword arguments:
+    url -- url of the article to write to file
+    file_name -- name of the file to which to write
+    """
+    f_ptr = open(file_name, 'w')
 
     try: 
         f = urllib.request.urlopen(url)
     except:
         return
+
     soup = BeautifulSoup(f.read())
     section = soup.find('div', {'class':'c75l'})
     
-    title = section.find('h1').getText()
+    try:
+        title = section.find('h1').getText()
+    except:
+        return
+
     body = section.findAll('p')
     print('\t\t' + title)
+    f_ptr.write('<DOC>\n')
     f_ptr.write('<DOC_NAME>' + title + '</DOC_NAME>\n')
     
     f_ptr.write('<TEXT>')
@@ -98,6 +130,7 @@ def write_article(url, f_ptr):
         f_ptr.write(para)
 
     f_ptr.write('</TEXT>\n')
+    f_ptr.write('</DOC>\n')
 
 if __name__ == "__main__":
     
